ad7463089fa3e51d1917fcefe77c06c63d24b8ed
sequoiap <sequoiap4@gmail.com>
2020-12-18 16:27:34 -0700

    Docstring fix for bode

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/utils/control.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/utils/control.py
@@ -4,17 +4,16 @@ import numpy as np
 def bode(*args, **kwargs):
     """
     Wraps the control library's ``bode`` function and passes all the parameters
-    straight through. I got tired of converting magnitude to dB and phase to
-    degrees after every function call, so now I just call my custom bode 
-    function that does it for me! Returns the same thing as ``control.bode``, 
-    except that now it's:
+    straight through. Acts more like MATLAB's ``bode`` function; it converts
+    magnitude to dB and phase to degrees after every function call. Returns the
+    same thing as ``control.bode``, except that now it's:
 
     Returns
     -------
-    mag : float
-        Magnitude in dB
-    phase : float
-        Phase in degrees
+    mag : float 
+        Magnitude in dB 
+    phase : float 
+        Phase in degrees 
     omega : np.array
         x-axis values over the range of ``mag`` and ``phase``.
     """
